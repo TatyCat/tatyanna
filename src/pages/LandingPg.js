e999c1ce67f02017009c436c8dda704f75b90cee
TatyCat <williams.tatyanna@gmail.com>
2019-04-04 14:14:12 -0400

    skills relocated to about and contact to satisfaction

--- src/pages/LandingPg.js
+++ src/pages/LandingPg.js
@@ -54,118 +54,38 @@ class LandingPg extends Component {
             <div className="w3-content w3-justify w3-text-grey w3-padding-64" id="about">
               <h2 className="w3-text-light-grey">Who am I?</h2>
               <hr className="width:200px" className="w3-opacity" />
-              <p>I am Tatyanna and I have a deep love for creating beautiful and cleanly written web and mobile applications. My enthusasm for creating is coupled with an interest in startup culture and a relentless work ethic. As a native of Tampa Bay, I enjoy attending tech and startup meetups. This  includes Code for Tampa Bay (a branch of Code for America), meetups sponsored by Suncoast Developers Guild, Startup Weekend, StartupBus 2018 and more.
+              <p>I am Tatyanna and I have a deep love for creating beautiful and cleanly written web and mobile applications. My enthusasm for creating is coupled with an interest in startup culture and a relentless work ethic. As a native of Tampa Bay, I enjoy attending tech and startup meetups. This  includes Code for Tampa Bay (a branch of Code for America), meetups sponsored by Suncoast Developers Guild, Startup Weekend, StartupBus 2018 and more. When I am not off attending a meetup, programming or volunteering, you can find me enjoying a light hearted anime or in a library enjoying a non-fiction book.
               </p>
+              <article className="skills">
+                <section className="top-tech-skills">
+                  <h3 className="w3-padding-16 w3-text-light-grey">Top Skills</h3>
+                  <p className="w3-wide">CSS <i class="fab fa-css3-alt"></i></p>
+                  <p className="w3-wide">JavaScript <i class="fab fa-js"></i></p>
+                  <p className="w3-wide">React <i class="fab fa-react"></i></p>
+                  <p className="w3-wide">Ruby <i class="far fa-gem"></i></p>
+                  <p className="w3-wide">PostgreSQL <i class="fas fa-database"></i></p>
+                  <p className="w3-wide">Rails <i class="far fa-gem"></i></p>
+                </section>
 
-              <h3 className="w3-padding-16 w3-text-light-grey">Top Skills</h3>
-              <p className="w3-wide">CSS</p>
-              <div className="w3-white">
-                <div className="w3-dark-grey" style={{ height: "28px", width: "85%" }}></div>
-              </div>
-              <p className="w3-wide">JavaScript</p>
-              <div className="w3-white">
-                <div className="w3-dark-grey" style={{ height: "28px", width: "55%" }}></div>
-              </div>
-              <p className="w3-wide">React</p>
-              <div className="w3-white">
-                <div className="w3-dark-grey" style={{ height: "28px", width: "60%" }}></div>
-              </div>
-
-              <p className="w3-wide">PostgreSQL</p>
-              <div className="w3-white">
-                <div className="w3-dark-grey" style={{ height: "28px", width: "50%" }}></div>
-              </div>
-              <p className="w3-wide">Ruby</p>
-              <div className="w3-white">
-                <div className="w3-dark-grey" style={{ height: "28px", width: "55%" }}></div>
-              </div>
-              <p className="w3-wide">Rails</p>
-              <div className="w3-white">
-                <div className="w3-dark-grey" style={{ height: "28px", width: "80%" }}></div>
-              </div>
-
-
-              <p className="w3-wide">Problem Solving</p>
-              <div className="w3-white">
-                <div className="w3-dark-grey" style={{ height: "28px", width: "90%" }}></div>
-              </div>
+                {/* <!-- Soft Skills --> */}
+                <section className="top-soft-skills">
+                  <h3 className="w3-padding-16 w3-text-light-grey">Top Soft Skills</h3>
+                  <p className="w3-wide">Empathy <i class="fas fa-check"></i></p>
+                  <p className="w3-wide">Problem Solving <i class="fas fa-check"></i></p>
+                </section>
+              </article>
 
               <br />
 
-              <div className="w3-row w3-center w3-padding-16 w3-section w3-light-grey">
-                <div className="w3-quarter w3-section">
-                  <span className="w3-xlarge">11+</span><br />
-                  Partners
-                </div>
-                <div className="w3-quarter w3-section">
-                  <span className="w3-xlarge">55+</span><br />
-                  Projects Done
-                </div>
-                <div className="w3-quarter w3-section">
-                  <span className="w3-xlarge">89+</span><br />
-                  Happy Clients
-                </div>
-                <div className="w3-quarter w3-section">
-                  <span className="w3-xlarge">150+</span><br />
-                  Meetings
-                </div>
-              </div>
-
-
-              <button className="w3-button w3-light-grey w3-padding-large w3-section">
-                <i className="fa fa-download"></i> Download Resume
-    </button>
-
-              {/* <!-- Grid for pricing tables --> */}
-              <h3 className="w3-padding-16 w3-text-light-grey">My Price</h3>
-              <div className="w3-row-padding" style={{ margin: "0 -16px" }}>
-                <div className="w3-half w3-margin-bottom">
-                  <ul className="w3-ul w3-white w3-center w3-opacity w3-hover-opacity-off">
-                    <li className="w3-dark-grey w3-xlarge w3-padding-32">Basic</li>
-                    <li className="w3-padding-16">Web Design</li>
-                    <li className="w3-padding-16">Photography</li>
-                    <li className="w3-padding-16">5GB Storage</li>
-                    <li className="w3-padding-16">Mail Support</li>
-                    <li className="w3-padding-16">
-                      <h2>$ 10</h2>
-                      <span className="w3-opacity">per month</span>
-                    </li>
-                    <li className="w3-light-grey w3-padding-24">
-                      <button className="w3-button w3-white w3-padding-large w3-hover-black">Sign Up</button>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="w3-half">
-                  <ul className="w3-ul w3-white w3-center w3-opacity w3-hover-opacity-off">
-                    <li className="w3-dark-grey w3-xlarge w3-padding-32">Pro</li>
-                    <li className="w3-padding-16">Web Design</li>
-                    <li className="w3-padding-16">Photography</li>
-                    <li className="w3-padding-16">50GB Storage</li>
-                    <li className="w3-padding-16">Endless Support</li>
-                    <li className="w3-padding-16">
-                      <h2>$ 25</h2>
-                      <span className="w3-opacity">per month</span>
-                    </li>
-                    <li className="w3-light-grey w3-padding-24">
-                      <button className="w3-button w3-white w3-padding-large w3-hover-black">Sign Up</button>
-                    </li>
-                  </ul>
-                </div>
-                {/* <!-- End Grid/Pricing tables --> */}
-              </div>
-
-              {/* <!-- Testimonials --> */}
-              <h3 className="w3-padding-24 w3-text-light-grey">My Reputation</h3>
-              <img src="/w3images/bandmember.jpg" alt="Avatar" className="w3-left w3-circle w3-margin-right" style={{ width: "80px" }} />
-              <p><span className="w3-large w3-margin-right">Chris Fox.</span> CEO at Mighty Schools.</p>
-              <p>Jane Doe saved us from a web disaster.</p><br />
-
-              <img src="/w3images/avatar_g2.jpg" alt="Avatar" className="w3-left w3-circle w3-margin-right" style={{ width: "80px" }} />
-              <p><span className="w3-large w3-margin-right">Rebecca Flex.</span> CEO at Company.</p>
-              <p>No one is better than Jane Doe.</p>
+              <p className="w3-center">
+                <button disabled className="w3-button w3-light-grey w3-padding-large w3-section">
+                  <a href={"https://www.-.com"} target="_blank"></a><i className="fa fa-download w3-disabled"></i> Download Resume
+                </button>
+              </p>
               {/* <!-- End About Section --> */}
             </div>
+
+            <br />
 
             {/* <!-- Portfolio Section --> */}
             <div className="w3-padding-64 w3-content" id="photos">
@@ -196,47 +116,65 @@ class LandingPg extends Component {
               <h2 className="w3-text-light-grey">Contact Me</h2>
               <hr style={{ width: "200px" }} className="w3-opacity" />
 
-              <div className="w3-section">
+              <p>If you believe I would be a great fit with your company,<br /> <span className="w3-large">let's get in touch:</span></p>
 
-
-              </div><br />
-
-              <p>Lets get in touch. Send me a message:</p>
-
-              <form action="/action_page.php" target="_blank">
+              <form action="#">
                 <p><input className="w3-input w3-padding-16" type="text" placeholder="Name" required name="Name" /></p>
                 <p><input className="w3-input w3-padding-16" type="text" placeholder="Email" required name="Email" /></p>
                 <p><input className="w3-input w3-padding-16" type="text" placeholder="Subject" required name="Subject" /></p>
                 <p><input className="w3-input w3-padding-16" type="text" placeholder="Message" required name="Message" /></p>
                 <p>
                   <button className="w3-button w3-light-grey w3-padding-large" type="submit">
-                    <i className="fa fa-paper-plane"></i> SEND MESSAGE
-                                    </button>
+                    <i className="fa fa-paper-plane w3-ruby"></i> SEND MESSAGE
+                </button>
                 </p>
               </form>
+              <br />
+
+
+              <div className="w3-section">
+                <div className="w3-row w3-center w3-padding-16 w3-section w3-border ">
+                  <div className="w3-quarter w3-section">
+                    <span className="w3-xlarge">
+                      <a href="https://twitter.com/DevTaty" target="_blank"><i className="fab fa-twitter w3-hover-opacity w3-hover-ruby"></i></a>
+                    </span>
+                    <br />
+                    Twitter
+                </div>
+                  <div className="w3-quarter w3-section">
+                    <span className="w3-xlarge">
+                      <a href="https://www.linkedin.com/in/tatyannac/" target="_blank"><i className=" fab fa-linkedin-in w3-hover-opacity w3-hover-ruby"></i></a>
+                    </span>
+                    <br />
+                    LinkedIn
+                </div>
+                  <div className="w3-quarter w3-section">
+                    <span className="w3-xlarge">
+                      <a href="mailto:tatyanna.cobb@gmail.com" target="_blank"><i className="w3-hover-opacity fas fa-envelope w3-hover-opacity w3-hover-ruby"></i></a>
+                    </span>
+                    <br />
+                    Email
+                </div>
+                  <div className="w3-quarter w3-section">
+                    <span className="w3-xlarge">
+                      <a href="https://paypal.me/DevTatyanna"><i className="fas fa-donate w3-hover-opacity w3-hover-ruby"></i>
+                      </a>
+                    </span>
+                    <br />
+                    Paypal
+                  </div>
+                </div>
+              </div>
+
               {/* <!-- End Contact Section --> */}
             </div>
 
-            {/* <!-- Footer --> */}
-            <footer className="w3-content w3-padding-64 w3-text-grey w3-xlarge">
-
-              <a href="https://twitter.com/DevTaty" target="_blank"><i className="w3-margin-right fab fa-twitter w3-hover-opacity"></i></a>
-
-              <a href="https://www.linkedin.com/in/tatyannac/" target="_blank"><i className="w3-margin-right fab fa-linkedin-in w3-hover-opacity"></i></a>
-
-              <a href="mailto:tatyanna.cobb@gmail.com" target="_blank"><i className="w3-hover-opacity w3-margin-right fas fa-envelope w3-hover-opacity"></i></a>
-
-              <a href="https://paypal.me/DevTatyanna"><i className="fas fa-donate w3-hover-opacity"></i>
-              </a>
-
-              {/* <!-- End footer --> */}
-            </footer>
 
             {/* <!-- END PAGE CONTENT --> */}
           </div>
 
 
-        </section>
+        </section >
       </>
 
     );
