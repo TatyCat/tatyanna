888529f9920827246c28e3a66d04443e799772d6
TatyCat <williams.tatyanna@gmail.com>
2019-04-27 13:42:54 -0400

    updated resume link

--- src/pages/LandingPg.js
+++ src/pages/LandingPg.js
@@ -123,7 +123,7 @@ class LandingPg extends Component {
               <br />
 
               <p className="w3-center">
-                <a href="https://drive.google.com/file/d/1IA9rgkxoA-Btwyfh647_EIwQh_ovaBhG/view?usp=sharing" target="_blank" rel="noopener noreferrer"><button className="w3-button w3-light-grey w3-padding-large w3-section">
+                <a href="https://drive.google.com/file/d/1Xi7AcKrFvnRqLtdvreJqobKKWbIIz7HA/view?usp=sharing" target="_blank" rel="noopener noreferrer"><button className="w3-button w3-light-grey w3-padding-large w3-section">
                   <i className="fa fa-download w3-disabled"></i> Download Resume
                 </button>
                 </a>
